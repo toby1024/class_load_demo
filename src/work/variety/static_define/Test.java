47725f9382f32dfe5432e357ef26e8b068b57323
张斌 <zhangbin@skio.cn>
2017-11-10 11:08:45 +0800

    静态语句块可以对定义在他后面的static变量进行赋值操作，但不能访问

---
+++ src/work/variety/static_define/Test.java
@@ -0,0 +1,16 @@
+package work.variety.static_define;
+
+public class Test {
+
+    static {
+        System.out.println("this is a test");
+        a = 456;
+//        System.out.println(a);
+    }
+
+    static int a = 123;
+
+    public static void main(String[] args) {
+        System.out.println("a---->" + a);
+    }
+}
